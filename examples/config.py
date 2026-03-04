4cf59aad1cd70d919a200fc9388e790827268378
Kan <wu-kan@163.com>
2026-03-04 15:34:56 +0800

    chore(docker-stable): prune branch to deployment-minimal layout

--- examples/config.py
+++
@@ -1,7 +0,0 @@
-# {time} 用来防止重放攻击
-# HTTP与WebSocket地址可以不同，也可以相同
-URL_WS = 'http://localhost:8080/api/v1/{time}'
-URL_HTTP = 'http://localhost:8080/api/v1/{time}'
-
-USERNAME = 'admin'
-PASSWORD = 'password123'
--- ksrpc/hack/__init__.py
+++
@@ -1,3 +0,0 @@
-import warnings
-
-warnings.warn('此库仅供学习交流，请在数据提供方的授权范围内使用。请勿向第三方转发数据')
